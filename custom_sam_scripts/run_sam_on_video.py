e55a11bb8fcae2f0e9ac22ca276eb4acd0dd3739
Nicolai Jørgensen <nicolai.jorgensen@envision-energy.com>
2024-10-14 16:27:31 +0200

    updated this demo script a bit more

--- custom_sam_scripts/run_sam_on_video.py
+++ custom_sam_scripts/run_sam_on_video.py
@@ -7,6 +7,7 @@ from typing import List, Union, Optional
 from pathlib import Path
 import argparse
 import torch
+import os
 
 relevant_dirs = add_dirs_to_path()
 
@@ -14,21 +15,27 @@ relevant_dirs = add_dirs_to_path()
 
 
 def sam_video_inference(
-        video_path_list: Union[str, Path, List[str], List[Path]],
+        video_path_list: Union[str, Path, List[str], List[Path]] = relevant_dirs.get("InfuSight_ENJYJS_SHL05", os.getcwd()),
         model_size: str = "large",
         output_dir: Optional[str] = None,
 ) -> None:
     
     # Get the list of single video filepaths
     video_path_list = get_list_of_single_filepaths(video_path_list=video_path_list)
+    video_path_list = [Path(video_path) for video_path in video_path_list]
 
+    # Build the SAM2 video predictor
     checkpoint = "./checkpoints/sam2.1_hiera_large.pt"
     model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
     predictor = build_sam2_video_predictor(model_cfg, checkpoint)
 
+    # Run the SAM2 video predictor for each video path
     for video_path in video_path_list:
-
+        if output_dir is None:
+            output_dir = video_path.parent
+        
         with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
+            
             state = predictor.init_state(video_path)
 
             # add new prompts and instantly get the output on the same frame
@@ -45,8 +52,8 @@ def sam_video_inference(
 ### Run from CLI 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--video_path", type=str, default="./data/video.mp4")
-    parser.add_argument("--output_dir", type=str, default="./output")
+    parser.add_argument("--video_path", type=str, default=relevant_dirs.get("InfuSight_ENJYJS_SHL05", os.getcwd()), help="Path to the video file or directory containing video files")
+    parser.add_argument("--output_dir", type=str, default=None, help="Path to the output directory")
     args = parser.parse_args()
 
     # Edit the input args 
