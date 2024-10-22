165c12234352b5fb6532c6d67fbeb29549b1d9b4
Nicolai Jørgensen <nicolai.jorgensen@envision-energy.com>
2024-10-22 11:22:16 +0200

    fixed the Jens preprocessing

--- custom_sam_scripts/preprocessing/Jens_test_division.py
+++ custom_sam_scripts/preprocessing/Jens_test_division.py
@@ -1,4 +1,8 @@
+import matplotlib
+matplotlib.use('TkAgg')
 from CameraAISharedLibrary.print_args_func import print_args
+from CameraAISharedLibrary.str2bool_func import str2bool
+from typing import List
 import cv2
 import numpy as np
 import argparse
@@ -6,7 +10,10 @@ from matplotlib import pyplot as plt
 import os
 
 
-def pixelwise_division(image1, image2):
+def pixelwise_division(
+        image1: np.ndarray,
+        image2: np.ndarray
+    ) -> np.ndarray:
     """Perform pixel-wise division of two images."""
     # Ensure the images have the same dimensions
     if image1.shape != image2.shape:
@@ -17,13 +24,18 @@ def pixelwise_division(image1, image2):
     return np.divide(image1.astype(np.float32), image2.astype(np.float32) + epsilon)
 
 
-def normalize_image(image):
+def normalize_image(image: np.ndarray) -> np.ndarray:
     """Normalize the image to the range [0, 255]."""
     norm_image = cv2.normalize(image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
     return norm_image.astype(np.uint8)
 
 
-def plot_and_save_images(images, titles, save_dir):
+def plot_and_save_images(
+        images: List[List[np.ndarray]],
+        titles: List[List[str]],
+        save_dir: str,
+        show_plot: bool
+    ) -> None:
     """
     Plot a grid of images with corresponding titles and save the plot.
     
@@ -31,6 +43,7 @@ def plot_and_save_images(images, titles, save_dir):
     images (list of lists): A 2D list containing the images to be plotted.
     titles (list of lists): A 2D list containing the titles for each image.
     save_dir (str): Directory to save the output plot.
+    show_plot (bool): Whether to show the plot.
     """
     # Create a subplot with the same dimensions as the images list
     fig, axs = plt.subplots(len(images), len(images[0]), figsize=(12, 18))
@@ -48,14 +61,20 @@ def plot_and_save_images(images, titles, save_dir):
     os.makedirs(save_dir, exist_ok=True)
     save_path = os.path.join(save_dir, "division_results.png")
     fig.savefig(save_path, bbox_inches='tight', dpi=300)
-    plt.show()
+    if show_plot:
+        plt.show(block=True)
     plt.close()
     return 
 
 
 
 
-def main(img1_path, img2_path, save_dir):
+def main(
+        img1_path: str,
+        img2_path: str,
+        save_dir: str,
+        show_plot: bool
+    ) -> None:
     # Load images
     image1_color = cv2.cvtColor(cv2.imread(img1_path, cv2.IMREAD_UNCHANGED), cv2.COLOR_BGR2RGB)
     image_1_gray = cv2.cvtColor(image1_color, cv2.COLOR_RGB2GRAY)
@@ -67,15 +86,15 @@ def main(img1_path, img2_path, save_dir):
     img2_over_img1 = pixelwise_division(image_2_gray, image_1_gray)
 
     # Normalize the resulting images
-    img_1_over_img2_normalized = normalize_image(img1_over_img2)
-    img_2_over_img1_normalized = normalize_image(img2_over_img1)
+    img1_over_img2_normalized = normalize_image(img1_over_img2)
+    img2_over_img1_normalized = normalize_image(img2_over_img1)
 
     # Define the images and titles
     images = [
         [image1_color, image2_color],
         [image_1_gray, image_2_gray],
         [img1_over_img2, img2_over_img1],
-        [img_1_over_img2_normalized, img_2_over_img1_normalized]
+        [img1_over_img2_normalized, img2_over_img1_normalized]
     ]
     titles = [
         ['Original Image 1', 'Original Image 2'],
@@ -85,7 +104,7 @@ def main(img1_path, img2_path, save_dir):
     ]
 
     # Call the plotting function with save_dir
-    plot_and_save_images(images, titles, save_dir)
+    plot_and_save_images(images=images, titles=titles, save_dir=save_dir, show_plot=show_plot)
 
 
 
@@ -93,17 +112,17 @@ def main(img1_path, img2_path, save_dir):
 ### Run from CLI 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Perform pixel-wise division on two images.")
-    parser.add_argument("--img1_path", default="/Image_Datasets/InfuSight/ENCXYN/SHL01/image_1.jpeg", help="Path to the first image")
-    parser.add_argument("--img2_path", default="/Image_Datasets/InfuSight/ENCXYN/SHL01/image_2.jpeg", help="Path to the second image")
-    parser.add_argument("--save_dir", default=None, help="Directory to save the output plot")
+    parser.add_argument("--img1_path", type=str, default="/Image_Datasets/InfuSight/ENCXYN/SHL01/image_1.jpeg", help="Path to the first image")
+    parser.add_argument("--img2_path", type=str, default="/Image_Datasets/InfuSight/ENCXYN/SHL01/image_2.jpeg", help="Path to the second image")
+    parser.add_argument("--save_dir", type=str, default=None, help="Directory to save the output plot")
+    parser.add_argument("--show_plot", type=str2bool, default=False, help="Whether to show the plot")
     args = parser.parse_args()
 
-    # Set default save_dir if not provided
-    if args.save_dir is None:
-        args.save_dir = os.path.dirname(os.path.commonprefix([args.img1_path, args.img2_path]))
+    # Edit the input args 
+    args.save_dir = os.path.dirname(args.img1_path) if "none" in str(args.save_dir).lower() else args.save_dir
 
     # Print the arguments
     print_args(args, init_str="This is the input args chosen when running Jens' preprocessing script")
 
     # Run the main function
-    main(args.img1_path, args.img2_path, args.save_dir)
+    main(args.img1_path, args.img2_path, args.save_dir, args.show_plot)
